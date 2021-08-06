74fd234553857c2723e1e827216be49d1fd35e6c
paragdev441 <85457553+paragdev441@users.noreply.github.com>
2021-08-06 18:43:50 +0530

    Add anchor for going to TextInPlace component

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,21 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <div>
       <h1>Welcome !</h1>
+      <Link to="/text-in-place">
+        <b>Go to TextInPlace</b>
+      </Link>
     </div>
   );
 };
 
 export default Welcome;
-
-/**
- * element.style {
-    display: flex;
-    justify-content: space-evenly;
-    background: radial-gradient(#21bcf9, #2f78b78c);
-    padding: 20px 0px;
-    width: 100%;
-}
- */
